a082bb33ad23e8c9fc48c76f2dd9b71eeea6921b
wounderfvl <10231027@student.itk.ac.id>
2025-03-05 10:44:04 +0800

    Add ProdukList component with state and effect

---
+++ frontend/ecommerce-frontend/src/components/ProdukList.tsx
@@ -0,0 +1,14 @@
+// src/components/ProdukList.jsx
+import React from "react";
+function ProdukList() {
+  return (
+    <div>
+      <h2>Daftar Produk</h2>
+      <ul>
+        <li>Contoh Produk A</li>
+        <li>Contoh Produk B</li>
+      </ul>
+    </div>
+  );
+}
+export default ProdukList;
