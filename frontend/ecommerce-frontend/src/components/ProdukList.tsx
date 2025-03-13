17cbd74c657641abc23155f07439507d3463f06a
wounderfvl <10231027@student.itk.ac.id>
2025-03-13 11:04:53 +0800

    04.1 update

--- frontend/ecommerce-frontend/src/components/ProdukList.tsx
+++ frontend/ecommerce-frontend/src/components/ProdukList.tsx
@@ -1,26 +1,32 @@
 // src/components/ProdukList.jsx
 import React, { useEffect, useState } from "react";
-
+import axios from "axios";
 interface Produk {
   id: number;
   nama: string;
+  harga: number;
 }
 
 function ProdukList() {
   const [produk, setProduk] = useState<Produk[]>([]);
   useEffect(() => {
-    // Data statis sementara
-    setProduk([
-      { id: 1, nama: "Azhka Daeshawnda" },
-      { id: 2, nama: "Ansell" },
-    ]);
+    axios
+      .get("http://localhost:3001/produk")
+      .then((response) => {
+        setProduk(response.data);
+      })
+      .catch((error) => {
+        console.error("Terjadi error:", error);
+      });
   }, []);
   return (
     <div>
-      <h2>Daftar Produk</h2>
+      <h2>Daftar Produk (From Database)</h2>
       <ul>
         {produk.map((item) => (
-          <li key={item.id}>{item.nama}</li>
+          <li key={item.id}>
+            {item.nama} - Rp{item.harga}
+          </li>
         ))}
       </ul>
     </div>
